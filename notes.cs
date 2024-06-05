6fd178e3b1c6fcf78f6e89d875ed1504f5e12bb5
kkspiridonova <kkspiridonova@yandex.ru>
2024-06-06 00:03:40 +0300

    dfgh

---
+++ notes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practocsharp8
+{
+    internal class notes
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string date;
+
+        public notes() { }
+        public notes(string title, string description, string date)
+        {
+            this.title = title;
+            this.description = description;
+            this.date = date;
+        }
+    }
+}
